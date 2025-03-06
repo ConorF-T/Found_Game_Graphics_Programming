8a8ef720d9f46d554c03c746b943f39d41ca0705
ConorF-T <conorfortunet@gmail.com>
2025-03-06 12:26:03 -0500

    Created a noise function

--- D3D11Starter-main/CustomPS.hlsl
+++ D3D11Starter-main/CustomPS.hlsl
@@ -28,6 +28,25 @@ float random(float2 s)
 	return frac(sin(dot(s, float2(12.9898, 78.233))) * 43758.5453123);
 };
 
+// Function to create a special pattern
+float2 truchetPattern(float2 s, float index)
+{
+	index = frac(((index - 0.5) * 2.0));
+	if (index > 0.75)
+	{
+		s = float2(1, 1) - s;
+	}
+	else if (index > 0.5) 
+	{
+		s = float2(1.0 - s.x, s.y);
+	}
+	else if (index > 0.25) 
+	{
+		s = 1.0 - float2(1.0 - s.x, s.y);
+	}
+	return s;
+};
+
 // --------------------------------------------------------
 // The entry point (main method) for our pixel shader
 // 
@@ -39,9 +58,19 @@ float random(float2 s)
 // --------------------------------------------------------
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	float2 st = input.screenPosition.xy;
+	float2 st = input.screenPosition.xy / input.uv.xy;
+	// Scalling everything to see the pattern better
+	st *= 10.0f;
 
-	float rand = random(st);
+	float2 ipos = floor(st);
+	float2 fpos = frac(st);
 
-	return float4(rand, rand, rand, 1.0f);
+	float2 tile = truchetPattern(fpos, random(ipos));
+
+	float colorFloat = 0.0f;
+
+	colorFloat = smoothstep(tile.x - 0.3, tile.x, tile.y) - smoothstep(tile.x, tile.x + 0.3, tile.y);
+
+
+	return float4(colorFloat, colorFloat, colorFloat, 1);
 }
